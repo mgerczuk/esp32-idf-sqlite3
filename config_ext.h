7fae750460a229f790e4fc7db6c3ea8fe5c54f1f
Martin <martin@gerczuk.de>
2020-12-21 13:51:30 +0100

    Same files as Arduino version

--- config_ext.h
+++ config_ext.h
@@ -1,31 +1,37 @@
 #define BUILD_sqlite -DNDEBUG
-#define SQLITE_CORE                          1
-#define SQLITE_NO_SYNC                       1
-#define YYSTACKDEPTH                        20
-#define SQLITE_TEMP_STORE                    1
-#define SQLITE_SYSTEM_MALLOC                 1
+#define SQLITE_OMIT_LOAD_EXTENSION           1
+#define SQLITE_DQS                           0
 #define SQLITE_OS_OTHER                      1
-#define SQLITE_THREADSAFE                    0
-#define SQLITE_MUTEX_APPDEF                  1
-#define SQLITE_SECURE_DELETE                 0
-#define SQLITE_SMALL_STACK                   1
+#define SQLITE_NO_SYNC                       1
+#define SQLITE_TEMP_STORE                    1
 #define SQLITE_DISABLE_LFS                   1
 #define SQLITE_DISABLE_DIRSYNC               1
+#define SQLITE_SECURE_DELETE                 0
+#define SQLITE_DEFAULT_LOOKASIDE        512,64
+#define YYSTACKDEPTH                        20
+#define SQLITE_SMALL_STACK                   1
+#define SQLITE_DEFAULT_PAGE_SIZE          4096
+#define SQLITE_SORTER_PMASZ                  4
+#define SQLITE_DEFAULT_CACHE_SIZE           -1
+#define SQLITE_DEFAULT_MEMSTATUS             0
+#define SQLITE_DEFAULT_MMAP_SIZE             0
+#define SQLITE_CORE                          1
+#define SQLITE_SYSTEM_MALLOC                 1
+#define SQLITE_THREADSAFE                    0
+#define SQLITE_MUTEX_APPDEF                  1
+#define SQLITE_OMIT_WAL                      1
 #define SQLITE_DISABLE_FTS3_UNICODE          1
 #define SQLITE_DISABLE_FTS4_DEFERRED         1
 #define SQLITE_LIKE_DOESNT_MATCH_BLOBS       1
-#define SQLITE_DEFAULT_CACHE_SIZE           -1
 #define SQLITE_DEFAULT_FOREIGN_KEYS          0
-#define SQLITE_DEFAULT_MEMSTATUS             0
-#define SQLITE_DEFAULT_MMAP_SIZE             0
 #define SQLITE_DEFAULT_LOCKING_MODE          1
-#define SQLITE_DEFAULT_LOOKASIDE       512,128
 #define SQLITE_DEFAULT_PAGE_SIZE          4096
 #define SQLITE_DEFAULT_PCACHE_INITSZ         8
 #define SQLITE_MAX_DEFAULT_PAGE_SIZE     32768
 #define SQLITE_POWERSAFE_OVERWRITE           1
-#define SQLITE_SORTER_PMASZ                  4
 #define SQLITE_MAX_EXPR_DEPTH                0
+
+/*
 #undef SQLITE_OMIT_ALTERTABLE
 #undef SQLITE_OMIT_ANALYZE
 #undef SQLITE_OMIT_ATTACH
@@ -55,7 +61,6 @@
 #define SQLITE_OMIT_INCRBLOB                 1
 #define SQLITE_OMIT_INTEGRITY_CHECK          1
 #undef SQLITE_OMIT_LIKE_OPTIMIZATION
-#define SQLITE_OMIT_LOAD_EXTENSION           1
 #undef SQLITE_OMIT_LOCALTIME
 #define SQLITE_OMIT_LOOKASIDE                1
 #undef SQLITE_OMIT_MEMORYDB
@@ -78,12 +83,12 @@
 #undef SQLITE_OMIT_VACUUM
 #undef SQLITE_OMIT_VIEW
 #undef SQLITE_OMIT_VIRTUALTABLE
-#define SQLITE_OMIT_WAL                      1
 #undef SQLITE_OMIT_WSD
 #define SQLITE_OMIT_XFER_OPT                 1
 #define SQLITE_PERFORMANCE_TRACE             1
-/* #define SQLITE_OMIT_COMPLETE              1 */
-/* #define SQLITE_OMIT_SUBQUERY              1 */
-/* #define SQLITE_OMIT_DATETIME_FUNCS        1 */
-/* #define SQLITE_OMIT_FLOATING_POINT        1 */
+//#define SQLITE_OMIT_COMPLETE              1
+//#define SQLITE_OMIT_SUBQUERY              1
+//#define SQLITE_OMIT_DATETIME_FUNCS        1
+//#define SQLITE_OMIT_FLOATING_POINT        1
 #define SQLITE_COUNTOFVIEW_OPTIMIZATION      0
+*/
